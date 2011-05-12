b3160979e28b73e19bf2c4d12a8049a93ab525be
samdong2 <findmobile@naver.com>
2011-05-13 08:19:30 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '22826c713a336609c62a479efa45fd201a749e2621c99f1060d2460afa0fec0127294d2a839869a59f16228db8f7ec63a7cddba667cb7e40b35caec89ae0b43b'
